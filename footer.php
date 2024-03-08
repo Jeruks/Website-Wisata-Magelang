0548f9cdd861b3d4c4f29a9577251c068b0d012f
Jeruks <ahsanadi1818@gmail.com>
2024-03-08 14:51:49 +0700

    styling carousel

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <hr>
 <footer class="container mt-5">
     <p class="float-end"><a href="#" class="fw-bold text-decoration-none ">Ke atas</a></p>
-    <p>&copy; 2024 Ahsan Adi Romza | SMK Ma`arif Walisongo Kajoran</p>
+    <p>&copy; 2024 Ahsan Adi Romza 🍊 | SMK Ma`arif Walisongo Kajoran</p>
 </footer>
 </main>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
